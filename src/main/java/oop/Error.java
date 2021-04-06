7419bcc8ebcd3ddd1e4a582d5f0a6d59bfbba14a
serkali88 <serkali88@gmail.com>
2021-04-06 12:47:59 +0400

    1.7. Конструктор[173889#510486]

---
+++ src/main/java/oop/Error.java
@@ -0,0 +1,33 @@
+package oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+    public void print(){
+        System.out.println("Message:"+active);
+        System.out.println("Message:"+status);
+        System.out.println("Message:"+message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        error.print();
+        Error error1 = new Error(true,5,"Error 404");
+        error1.print();
+        Error error2 = new Error(false,4,"No faled");
+        error2.print();
+        Error error3 = new Error(false,3,"ERROR");
+        error3.print();
+
+    }
+}
