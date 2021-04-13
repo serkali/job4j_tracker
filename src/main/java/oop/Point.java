73150c44a93542f5891cb6fcb414ac4cd8bdfdbd
serkali88 <serkali88@gmail.com>
2021-04-13 19:37:04 +0400

    Реализация профессий в коде[6837#510496]

--- src/main/java/oop/Point.java
+++ src/main/java/oop/Point.java
@@ -3,6 +3,7 @@ package oop;
 public class Point {
     private int x;
     private int y;
+    private int z;
 
     public Point() {
     }
@@ -11,16 +12,24 @@ public class Point {
         this.x = first;
         this.y = second;
     }
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
 
     public static void main(String[] args) {
-        Point point = new Point(0, 0);
-        Point point1 = new Point(0, 2);
-        double dist = point.distance(point1);
+        Point point = new Point(0, 0,0);
+        Point point1 = new Point(0, 2,2);
+        double dist = point.distance3d(point1);
         System.out.println(dist);
     }
 
     public double distance(Point that) {
         return Math.sqrt(Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2));
+    }
+    public double distance3d(Point that) {
+        return Math.sqrt(Math.pow(this.x- that.x,2)+Math.pow(this.y-that.y,2)+Math.pow(this.z-that.z,2));
     }
 
 }
