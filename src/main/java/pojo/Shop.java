90ae6b2f9c518b454283a52d22bfad2477f05a32
serkali88 <serkali88@gmail.com>
2021-04-18 23:31:40 +0400

    3.1. Массив с пустыми ячейками[176470#510501]

---
+++ src/main/java/pojo/Shop.java
@@ -0,0 +1,17 @@
+package pojo;
+
+public class Shop {
+    public static int indexOfNull(Product[] products) {
+        for (int i = 0; i < products.length; i++) {
+            if (products[i] == null) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+
+
+
+
+
