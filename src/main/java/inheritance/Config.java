2e7aa2feb67b1d91710469804b9958ea5c691219
serkali88 <serkali88@gmail.com>
2021-04-18 15:14:20 +0400

    Что такое инкапсуляция.[3818#510508]

---
+++ src/main/java/inheritance/Config.java
@@ -0,0 +1,23 @@
+package inheritance;
+
+public class Config {
+    private String[] properties;
+    private int position;
+    private String name;
+
+    public Config(String name) {
+        this.name = name;
+    }
+
+    public void print() {
+        System.out.println(position);
+    }
+
+    public String getProperty(String key) {
+        return search(key);
+    }
+
+    public String search(String key) {
+        return key;
+    }
+}
