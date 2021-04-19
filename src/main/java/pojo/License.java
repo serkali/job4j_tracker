b6b8bf7fb4281406fcda775113d4dfb912579183
serkali88 <serkali88@gmail.com>
2021-04-19 20:07:33 +0400

    Сравнение моделей. Метод equals[174893#510500]

--- src/main/java/pojo/License.java
+++ src/main/java/pojo/License.java
@@ -1,35 +1,33 @@
 package pojo;
 
 import java.util.Date;
+import java.util.Objects;
 
 public class License {
     private String owner;
     private String model;
     private String code;
+    private Date created;
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         License license = (License) o;
-
-        if (owner != null ? !owner.equals(license.owner) : license.owner != null) return false;
-        if (model != null ? !model.equals(license.model) : license.model != null) return false;
-        if (code != null ? !code.equals(license.code) : license.code != null) return false;
-        return created != null ? created.equals(license.created) : license.created == null;
+        return Objects.equals(owner, license.owner)
+                && Objects.equals(model, license.model)
+                && Objects.equals(code, license.code) && Objects.equals(created, license.created);
     }
 
     @Override
-    public int hashCode() {
-        int result = owner != null ? owner.hashCode() : 0;
-        result = 31 * result + (model != null ? model.hashCode() : 0);
-        result = 31 * result + (code != null ? code.hashCode() : 0);
-        result = 31 * result + (created != null ? created.hashCode() : 0);
-        return result;
-    }
 
-    private Date created;
+    public int hashCode() {
+        return Objects.hash(owner, model, code, created);
+    }
 
     public String getOwner() {
         return owner;
