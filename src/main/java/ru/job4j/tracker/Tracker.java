69de67fc2fad2045fd7e4e016cbf39755b193b94
serkali88 <serkali88@gmail.com>
2021-05-15 15:24:36 +0400

    5. Tracker - хранилище[396#510507]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -14,29 +14,14 @@ public class Tracker {
     }
 
     public Item findById(int id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
-                break;
-            }
-        }
-        return rsl;
+        /* Находим индекс */
+        int index = indexOf(id);
+        /* Если индекс найден возвращаем item, иначе null */
+        return index != -1 ? items[index] : null;
     }
 
     public Item[] findAll() {
-        Item[] res = new Item[items.length];
-        int count = 0;
-        for (int i = 0; i < size; i++) {
-            Item item = items[i];
-            if (item.getName() != null) {
-                items[count] = item;
-                count++;
-            }
-        }
-        res = Arrays.copyOf(items, count);
-        return res;
+        return Arrays.copyOf(items,size);
     }
 
     public Item[] findByName(String key) {
@@ -44,12 +29,31 @@ public class Tracker {
         int count = 0;
         for (int index = 0; index < size; index++) {
             Item item = items[index];
-            if (item.getName() == key) {
+            if (item.getName().equals(key)) {
                 items[count] = item;
                 count++;
             }
         }
         res = Arrays.copyOf(items, count);
         return res;
+    }
+
+    private int indexOf(int id) {
+        int rsl = -1;
+        for (int index = 0; index < size; index++) {
+            if (items[index].getId() == id) {
+                rsl = index;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public boolean replace(int id, Item item) {
+        int index = indexOf(id);
+        items[index] = item;
+        if (index = item)
+
+
     }
 }
