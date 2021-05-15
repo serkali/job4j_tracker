fbb91303dcdd07078c93ae87608584b3236bc949
serkali88 <serkali88@gmail.com>
2021-05-15 13:50:39 +0400

    5. Tracker - хранилище[396#510507]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,33 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findAll() {
+        Item[] res = new Item[items.length];
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            Item item = items[i];
+            if (item.getName() != null) {
+                items[count] = item;
+                count++;
+            }
+        }
+        res = Arrays.copyOf(items, count);
+        return res;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] res = new Item[items.length];
+        int count = 0;
+        for (int index = 0; index < size; index++) {
+            Item item = items[index];
+            if (item.getName() == key) {
+                items[count] = item;
+                count++;
+            }
+        }
+        res = Arrays.copyOf(items, count);
+        return res;
     }
 }
