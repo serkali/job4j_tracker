4704f92fe6044182c3fe9c0830015531b6bf95e0
serkali88 <serkali88@gmail.com>
2021-05-16 11:27:40 +0400

    7. Метод удаления заявки Tracker.delete[211749#510503]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -4,10 +4,11 @@ import java.time.format.DateTimeFormatter;
 
 public class StartUI {
     public static void main(String[] args) {
-        Item item = new Item();
+        Item item = new Item(1, "Heizenberg");
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String date = item.getCreated().format(formatter);
         System.out.println(date);
+        System.out.println(item);
 
     }
 }
